be519ae5ca8c4ccfc1fb3597873b463e6b17a7db
kvnlay <kingstevens007@gmail.com>
2019-07-26 16:47:22 +0100

    Add helper file

---
+++ src/helper.js
@@ -0,0 +1,17 @@
+const selectRandom = space => Math.floor(Math.random() * space);
+
+const getCoodinates = () => {
+    const coodinates = [selectRandom(10), selectRandom(10)];
+    return coodinates;
+};
+
+const randomDirection = () => {
+    const dir = selectRandom(2) === 0 ? 'x' : 'y';
+    return dir;
+};
+
+export {
+    selectRandom,
+    getCoodinates,
+    randomDirection,
+};
