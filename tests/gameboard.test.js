8e16ea34d25bc41d1751174d92d8ccafb61c9474
kvnlay <kingstevens007@gmail.com>
2019-07-27 18:16:03 +0100

    Add tests to check if all ships are sunk

--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -62,7 +62,9 @@ test('ships can\'t overlap on the board and both ships are on the board', () => 
 
 test('attack should miss if no ship in coordinate', () => {
   const newGame = gameboardFactory();
-  expect(newGame.receiveAttack(0, 0)).toBeFalsy;
+  expect(newGame.receiveAttack(0, 0)).toBe(false);
+  newGame.receiveAttack(0, 2);
+  expect(newGame.gameboard[0][2]).toMatch('Miss');
 });
 
 test('attack should hit ship', () => {
@@ -77,4 +79,45 @@ test('attack should hit ship', () => {
   expect(newGame.receiveAttack(0, 0)).toBe(true);
   expect(ship.hit).toHaveBeenCalled();
   expect(newGame.gameboard[0][0]).toMatch('Hit');
+});
+
+test('All ships should be sunk', () => {
+  const newGame = gameboardFactory();
+  const isSunk = jest.fn(() => true);
+  const shipOne = {
+    length: 4,
+    hitCoords: [],
+    coordinates: [],
+    isSunk,
+  };
+
+  const shipTwo = {
+    length: 5,
+    hitCoords: [],
+    coordinates: [],
+    isSunk,
+  };
+  newGame.placeShip(shipOne, [0, 0], 'x');
+  newGame.placeShip(shipTwo, [5, 0], 'y');
+  expect(newGame.isAllSunk()).toBe(true);
+});
+
+test('Not All ships are sunk', () => {
+  const newGame = gameboardFactory();
+  const shipOne = {
+    length: 4,
+    hitCoords: [],
+    coordinates: [],
+    isSunk: jest.fn(() => true),
+  };
+
+  const shipTwo = {
+    length: 5,
+    hitCoords: [],
+    coordinates: [],
+    isSunk: jest.fn(() => false),
+  };
+  newGame.placeShip(shipOne, [0, 0], 'x');
+  newGame.placeShip(shipTwo, [5, 0], 'y');
+  expect(newGame.isAllSunk()).toBe(false);
 });
