ca4b422242eca14ce9033d6540b5870b7d6bd122
kvnlay <kingstevens007@gmail.com>
2019-07-30 11:03:56 +0100

    Add render board and display message methods

--- src/domModule.js
+++ src/domModule.js
@@ -1,1 +1,30 @@
-const 
+const domModule = (() => {
+  const renderBoard = (owner, matrix) => {
+    const parent = document.getElementById('container');
+    const container = document.createElement('div');
+    container.classList.add(`${owner}-board`);
+    for (let i = 0; i < matrix.length; i++) {
+      for (let j = 0; j < matrix[i].length; j++) {
+        const cell = document.createElement('div');
+        cell.classList.add('board-cell');
+        cell.setAttribute('data-index', `${owner}-${i}${j}`);
+        if (owner === 'human') {
+          if (typeof matrix[i][j] === 'object') {
+            cell.classList.add('ship');
+          }
+        }
+      }
+    }
+    parent.appendChild(container);
+  };
+
+  const displayMessage = (msg) => {
+    const message = document.getElementById('message');
+    message.textContent = msg;
+  };
+
+  return {
+    renderBoard,
+    displayMessage,
+  };
+})();
