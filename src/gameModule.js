0dbd81ecd750bdf28403ca46faed2297c486cbec
Musa Jabbaaru Ntege <cenajab@gmail.com>
2019-07-30 16:58:35 +0300

    create boards in gameModule

--- src/gameModule.js
+++ src/gameModule.js
@@ -1,3 +1,32 @@
+import shipFactory from './shipFactory';
+import gameboardFactory from './gameboardFactory';
+import player from './player';
+import { getCoodinates } from './helper';
+
 const gameModule = (() => {
 
+  const carrier1 = shipFactory(5);
+  const carrier2 = shipFactory(5);
+  const battleship1 = shipFactory(4);
+  const battleship2 = shipFactory(4);
+  const destroyer1 = shipFactory(3);
+  const destroyer2 = shipFactory(3);
+  const submarine1 = shipFactory(3);
+  const submarine2 = shipFactory(3);
+  const patrol1 = shipFactory(2);
+  const patrol2 = shipFactory(2);
+
+  const humanBoard = gameboardFactory.gameboard;
+  humanBoard.placeShip(carrier1);
+  humanBoard.placeShip(battleship1);
+  humanBoard.placeShip(destroyer1);
+  humanBoard.placeShip(submarine1);
+  humanBoard.placeShip(patrol1);
+
+  const computerBoard = gameboardFactory.gameboard;
+  computerBoard.placeShip(carrier2);
+  computerBoard.placeShip(battleship2);
+  computerBoard.placeShip(destroyer2);
+  computerBoard.placeShip(submarine2);
+  computerBoard.placeShip(patrol2);
 })();
