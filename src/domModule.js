e18f88b1917092fd787b6fcadeb6f6ddb846fc2e
kvnlay <kingstevens007@gmail.com>
2019-07-31 11:41:06 +0100

    refactor element selector from data-index to id

--- src/domModule.js
+++ src/domModule.js
@@ -1,14 +1,14 @@
 const domModule = (() => {
-  const renderBoard = (owner, matrix) => {
-    const parent = document.getElementById('container');
+  const renderBoard = (parent, owner, matrix) => {
     const container = document.createElement('div');
-    container.classList.add(`${owner}-board`);
+    container.classList.add(`${owner.name}-board`);
     for (let i = 0; i < matrix.length; i++) {
       for (let j = 0; j < matrix[i].length; j++) {
         const cell = document.createElement('div');
         cell.classList.add('board-cell');
-        cell.setAttribute('data-index', `${owner}-${i}${j}`);
-        if (owner === 'human') {
+        cell.setAttribute('id', `${owner.name}-${i}${j}`);
+        container.appendChild(cell);
+        if (owner.name === 'human') {
           if (typeof matrix[i][j] === 'object') {
             cell.classList.add('ship');
           }
@@ -33,3 +33,5 @@ const domModule = (() => {
     disableCell,
   };
 })();
+
+export default domModule;
